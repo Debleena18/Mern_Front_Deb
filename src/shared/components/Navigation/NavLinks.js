7d9ea633388a04f16379c875cee0db616497074e
Debleena18 <sarkardebleena18@gmail.com>
2021-06-17 15:50:40 +0530

    first

---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import './NavLinks.css';
+
+const NavLinks= props => {
+    return <ul className="nav-links">
+        <li>
+            <NavLink to="/" exact>Everyone</NavLink>
+        </li>
+        <li>
+            <NavLink to="/u1/places">Only Mine</NavLink>
+        </li>
+        <li>
+            <NavLink to="/places/new">Friends</NavLink>
+        </li>
+        <li>
+            <NavLink to="/auth">Authenticate</NavLink>
+        </li>
+    </ul>
+}
+
+export default NavLinks;
